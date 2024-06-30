ffbbd3a5d360625c3b7bbbfdf6c06b666b80a3c2
Nicolas Zalasik <nizalasik@gmail.com>
2024-06-29 21:07:47 -0300

    Projeto 90%

--- script.js
+++ script.js
@@ -9,7 +9,12 @@ const cartCounter = document.getElementById("cart-count")
 const addresInput = document.getElementById("address")
 const addressWarn = document.getElementById("address-warn")
 const observationInput = document.getElementById("observacao")
-
+const nameInput = document.getElementById("name-input")
+const nameInputWarn = document.getElementById("name-input-warn")
+const pagamentoInputs = document.getElementsByName("forma-pagamento");
+const pagamentoWarn = document.getElementById("pagament-input-warn");
+const houseNumber = document.getElementById("house-number");
+const houseNumberWarn = document.getElementById("house-number-warn");
 let cart = [];
 
 // Abrir modal do carrinho
@@ -29,7 +34,7 @@ closeModalBtn.addEventListener("click", function(){
     cartModal.style.display = 'none'
 })
 
-// Pega 
+// Pegar item do menu
 menu.addEventListener("click", function(event){
     let parentButton = event.target.closest(".add-to-cart-btn")
     if(parentButton){
@@ -39,30 +44,25 @@ menu.addEventListener("click", function(event){
     }
 })
 
-// Função para adicionar no carrinho
+// Adicionar item ao carrinho
 function addToCart(name, price){
     const existingItem = cart.find(item => item.name === name)
     if(existingItem){
-        //Se o item já existe aumenta a quantidade +1
         existingItem.quantity += 1;
-    
-    }else{
-
-    cart.push({
-        name,
-        price,
-        quantity: 1,
-    })
+    } else {
+        cart.push({
+            name,
+            price,
+            quantity: 1,
+        })
     }
     updateCartModal()
 }
 
-
-// Atualiza o carrinho
+// Atualizar modal do carrinho
 function updateCartModal() {
     cartItemsContainer.innerHTML = "";
     let total = 0;
-
 
     cart.forEach(item => {
        const cartItemElement = document.createElement("div");
@@ -76,17 +76,17 @@ function updateCartModal() {
             <p class="font-medium">R$ ${item.price.toFixed(2)}</p>
             </div>
 
-                <button class="remove-from-cart-btn" data-name="${item.name}">
-                Remover
-                </button>
+            <button class="remove-from-cart-btn" data-name="${item.name}">
+            Remover
+            </button>
         </div>       
        `
         total += item.price * item.quantity;
 
         cartItemsContainer.appendChild(cartItemElement)
         document.getElementById('cart-total').innerText = total.toFixed(2);
+        document.getElementById('cart-total-pgto').innerText = total.toFixed(2);
         document.getElementById('footer-cart-total').innerText = total.toFixed(2)
-
     })
 
     cartTotal.textContent = total.toLocaleString("pt-BR", {
@@ -96,11 +96,10 @@ function updateCartModal() {
     cartCounter.innerHTML = cart.length;
 }
 
-// Função para remover item do carrinho
+// Remover item do carrinho
 cartItemsContainer.addEventListener("click", function (event){
     if(event.target.classList.contains("remove-from-cart-btn")){
         const name = event.target.getAttribute("data-name")
-
         removeItemCart(name);
     }
 })
@@ -121,7 +120,7 @@ function removeItemCart(name){
     }
 }
 
-// Função de captação de endereço
+// Capturar endereço
 addresInput.addEventListener("input", function(event){
     let inputValue = event.target.value;
 
@@ -131,6 +130,56 @@ addresInput.addEventListener("input", function(event){
     }
 })
 
+// Capturar número da casa
+houseNumber.addEventListener("input", function(event) {
+    let inputValue = event.target.value.trim();
+
+    if (inputValue !== "") {
+        houseNumber.classList.remove("border-red-500");
+        houseNumberWarn.classList.add("hidden");
+    } else {
+        houseNumber.classList.add("border-red-500");
+        houseNumberWarn.classList.remove("hidden");
+    }
+});
+
+// Capturar nome
+nameInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        nameInput.classList.remove("border-red-500")
+        nameInputWarn.classList.add("hidden") 
+    }
+})
+
+// Validar input
+function handleInputValidation(inputElement, warningElement) {
+    inputElement.addEventListener("input", function(event) {
+        let inputValue = event.target.value;
+
+        if (inputValue !== "") {
+            inputElement.classList.remove("border-red-500");
+            warningElement.classList.add("hidden");
+        }
+    });
+}
+
+// Aplicar função para o campo de nome
+handleInputValidation(nameInput, nameInputWarn,);
+
+// Validar forma de pagamento
+function validatePayment() {
+    let isPaymentSelected = false;
+
+    pagamentoInputs.forEach(input => {
+        if (input.checked) {
+            isPaymentSelected = true;
+        }
+    });
+
+    return isPaymentSelected;
+}
 
 // Finalizar pedido
 checkoutBtn.addEventListener("click", function(){
@@ -147,7 +196,6 @@ checkoutBtn.addEventListener("click", function(){
                 background: "#EF4444",
             },
         }).showToast();
-
         return;
     }
 
@@ -157,29 +205,75 @@ checkoutBtn.addEventListener("click", function(){
         addresInput.classList.add("border-red-500")
         return;
     }
+    if(nameInput.value === ""){
+        nameInputWarn.classList.remove("hidden")
+        nameInput.classList.add("border-red-500")
+        return;
+    }
+    if(houseNumber.value === ""){
+        houseNumberWarn.classList.remove("hidden")
+        houseNumber.classList.add("border-red-500")
+        return;
+    }
 
-    // Enviar pedido para whatsapp
+    // Verificar se o total é maior ou igual a R$ 20,00
+    const totalAmount = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+    if (totalAmount < 20) {
+        Toastify ({
+            text: "O valor mínimo do pedido deve ser de R$ 20,00",
+            duration: 3000,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "center", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "#EF4444",
+            },
+        }).showToast();
+        return;
+    }
+
+    // Validar forma de pagamento
+    if (!validatePayment()) {
+        pagamentoWarn.classList.remove("hidden");
+        return;
+    }
+
+    // Montar mensagem para enviar via WhatsApp
     const cartItems = cart.map((item) => {
         return (
-            `${item.name} - (${item.quantity}) Preço: R$ ${item.price} | `
-    )
-    }).join("")
+            `➡ ${item.name}: ${item.quantity} un. \n       Valor: R$ ${item.price.toFixed(2)}\n\n`
+        )
+    }).join("");
+
+    const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0).toFixed(2);
 
     const greeting = getGreeting(); // Obter saudação
-    const message = encodeURIComponent(`🤖 ${greeting}, aqui estão os itens do seu pedido: ${cartItems}`);
-    const phone = "5547991884707"
+    const selectedPayment = Array.from(pagamentoInputs).find(input => input.checked).id;
 
-    window.open(`https://wa.me/${phone}?text=${message} *Endereço:* ${addresInput.value} *Observação:* ${observationInput.value}`, "_blank")
-    // Limpar sacola, observação e endereço
+    const message = encodeURIComponent(`🤖 ${greeting}. Me chamo *${nameInput.value}*, aqui estão os itens do meu pedido: \n\n${cartItems}\nTotal: R$ ${total}\nForma de pagamento: ${selectedPayment}
+    \n🛵 *Endereço:* ${addresInput.value}, ${houseNumber.value} \n📋 *Observação:* ${observationInput.value}\n🏦 *Forma de pagamento:* ${selectedPayment}`);
+    
+    const phone = "5547991884707";
+    const whatsappLink = `https://wa.me/${phone}?text=${message}`;
+
+    // Redirecionar para o link do WhatsApp
+    window.open(whatsappLink, "_blank");
+
+    // Limpar carrinho, observação e endereço
     cart.length = 0;
     addresInput.value = "";
     observationInput.value = "";
+    pagamentoInputs.forEach(input => {
+        input.checked = false;
+    });
+
     // Fechar modal
     closeModalBtn.click();
     updateCartModal();
-})
+});
 
-// Verificar hora
+// Verificar se o restaurante está aberto
 function checkRestaurantOpen() {
     const data = new Date();
     const hora = data.getHours();
@@ -187,18 +281,7 @@ function checkRestaurantOpen() {
     return (hora >= 18 && (hora < 23 || (hora === 23 && minutos < 30))); // true
 }
 
-const spanItem = document.getElementById("date-span")
-const isOpen = checkRestaurantOpen();
-
-if(isOpen){
-    spanItem.classList.remove("bg-red-500");
-    spanItem.classList.add("bg-green-600")
-}else{
-    spanItem.classList.remove("bg-green-600")
-    spanItem.classList.add("bg-red-500")
-}
-
-// Função para determinar a saudação baseada no horário
+// Determinar saudação com base no horário
 function getGreeting() {
     const now = new Date();
     const hour = now.getHours();
